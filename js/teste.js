23c06ff862d519caf21da9245aebed635968c6b1
tiagoluis86 <70555750+tiagoluis86@users.noreply.github.com>
2021-12-29 08:36:28 -0300

    Add files via upload

---
+++ js/teste.js
@@ -0,0 +1,20 @@
+function sum (a, b) {
+    return a + b;
+
+}
+
+console.log(sum(3, 4));
+
+
+var tiago = 34
+
+console.log (tiago)
+
+tiago: {
+    idade = 34,
+    altura = 168,
+    peso = 62
+}
+
+console.log ('Tiago tem as seguintes características ' + idade, altura, peso)
+
